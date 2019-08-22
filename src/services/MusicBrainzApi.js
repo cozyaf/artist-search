89312d10c53fb7de22ee92e5f94b962916f29cd2
sepuckett86 <sepuckett86@gmail.com>
2019-08-21 17:39:22 -0700

    Api functions for getArtist, getReleases, and getArtwork

--- src/services/MusicBrainzApi.js
+++ src/services/MusicBrainzApi.js
@@ -6,3 +6,28 @@ export const getArtists = (search) => {
     });
 };
 
+export const getArtist = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist/${id}?inc=aliases&fmt=json`)
+    .then(res => {
+      if(!res.ok) throw 'Unable to fetch stuff bruh :(';
+      return res.json();
+    });
+};
+
+export const getReleases = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
+    .then(res => {
+      if(!res.ok) throw 'Unable to fetch stuff bruh :(';
+      return res.json();
+    });
+};
+
+export const getArtwork = (releaseId) => {
+  return fetch(`http://coverartarchive.org/release/${releaseId}/front`)
+    .then(res => {
+      if(!res.ok) throw 'Unable to fetch stuff bruh :(';
+      return res.json();
+    });
+};
+
+
